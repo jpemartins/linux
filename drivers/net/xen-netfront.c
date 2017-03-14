e375cb63640a0570815b8d80b4c70cbb9d1af92a
Joao Martins <joao.m.martins@oracle.com>
2017-03-14 16:47:29 +0000

    xen-netfront: move rx_gso_checksum_fixup to netfront_stats
    It allows us to remove one atomic op (on a very rare case) and
    further allow easier adding of new statistics. For this purpose
    we move rx_gso_checksum_fixup to the existent struct netfront_stats and
    adjust xennet_stats accordingly.
    
    After this patch, New netfront specific stats will simply need to get
    added into netfront_stats and xennet_stats.
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>

--- drivers/net/xen-netfront.c
+++ drivers/net/xen-netfront.c
@@ -93,10 +93,14 @@ struct netfront_cb {
 /* IRQ name is queue name with "-tx" or "-rx" appended */
 #define IRQ_NAME_SIZE (QUEUE_NAME_SIZE + 3)
 
+#define XENNET_STAT(field) #field,offsetof(struct netfront_stats, field)
+
 struct netfront_stats {
 	u64			packets;
 	u64			bytes;
 	struct u64_stats_sync	syncp;
+
+	u64	rx_gso_checksum_fixup;
 };
 
 struct netfront_info;
@@ -190,8 +194,6 @@ struct netfront_info {
 	/* Statistics */
 	struct netfront_stats __percpu *rx_stats;
 	struct netfront_stats __percpu *tx_stats;
-
-	atomic_t rx_gso_checksum_fixup;
 };
 
 struct netfront_rx_info {
@@ -957,7 +959,7 @@ static RING_IDX xennet_fill_frags(struct netfront_queue *queue,
 	return cons;
 }
 
-static int checksum_setup(struct net_device *dev, struct sk_buff *skb)
+static int checksum_setup(struct netfront_info *info, struct sk_buff *skb)
 {
 	bool recalculate_partial_csum = false;
 
@@ -968,8 +970,9 @@ static int checksum_setup(struct net_device *dev, struct sk_buff *skb)
 	 * recalculate the partial checksum.
 	 */
 	if (skb->ip_summed != CHECKSUM_PARTIAL && skb_is_gso(skb)) {
-		struct netfront_info *np = netdev_priv(dev);
-		atomic_inc(&np->rx_gso_checksum_fixup);
+		struct netfront_stats *rx_stats = this_cpu_ptr(info->rx_stats);
+
+		rx_stats->rx_gso_checksum_fixup++;
 		skb->ip_summed = CHECKSUM_PARTIAL;
 		recalculate_partial_csum = true;
 	}
@@ -998,7 +1001,7 @@ static int handle_incoming_queue(struct netfront_queue *queue,
 		skb->protocol = eth_type_trans(skb, queue->info->netdev);
 		skb_reset_network_header(skb);
 
-		if (checksum_setup(queue->info->netdev, skb)) {
+		if (checksum_setup(queue->info, skb)) {
 			kfree_skb(skb);
 			packets_dropped++;
 			queue->info->netdev->stats.rx_errors++;
@@ -2448,10 +2451,7 @@ static const struct xennet_stat {
 	char name[ETH_GSTRING_LEN];
 	u16 offset;
 } xennet_stats[] = {
-	{
-		"rx_gso_checksum_fixup",
-		offsetof(struct netfront_info, rx_gso_checksum_fixup)
-	},
+	{ XENNET_STAT(rx_gso_checksum_fixup) },
 };
 
 static int xennet_get_sset_count(struct net_device *dev, int string_set)
@@ -2467,11 +2467,16 @@ static int xennet_get_sset_count(struct net_device *dev, int string_set)
 static void xennet_get_ethtool_stats(struct net_device *dev,
 				     struct ethtool_stats *stats, u64 * data)
 {
-	void *np = netdev_priv(dev);
-	int i;
+	struct netfront_info *np = netdev_priv(dev);
+	int i, cpu;
+	void *st;
 
-	for (i = 0; i < ARRAY_SIZE(xennet_stats); i++)
-		data[i] = atomic_read((atomic_t *)(np + xennet_stats[i].offset));
+	for (i = 0; i < ARRAY_SIZE(xennet_stats); i++) {
+		for_each_possible_cpu(cpu) {
+			st = per_cpu_ptr(np->rx_stats, cpu);
+			data[i] += *((u64 *)(st + xennet_stats[i].offset));
+		}
+	}
 }
 
 static void xennet_get_strings(struct net_device *dev, u32 stringset, u8 * data)
