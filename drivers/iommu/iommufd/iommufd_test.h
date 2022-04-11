5bb85f49fab7e64bf9a5d333edfc5cea1484efe0
Joao Martins <joao.m.martins@oracle.com>
2022-04-11 09:20:08 -0400

    iommufd/selftest: Test IOMMU_HWPT_GET_DIRTY_IOVA
    Add a new test ioctl for simulating the dirty IOVAs in the mock domain, and
    implement the mock iommu domain ops that get the dirty tracking supported.
    
    The selftest exercises the usual main workflow of:
    
    1) Setting dirty tracking from the iommu domain
    2) Read and clear dirty IOPTEs
    
    Different fixtures will test different IOVA range sizes, that exercise
    corner cases of the bitmaps.
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>

--- drivers/iommu/iommufd/iommufd_test.h
+++ drivers/iommu/iommufd/iommufd_test.h
@@ -18,6 +18,7 @@ enum {
 	IOMMU_TEST_OP_ACCESS_RW,
 	IOMMU_TEST_OP_SET_TEMP_MEMORY_LIMIT,
 	IOMMU_TEST_OP_MOCK_DOMAIN_REPLACE,
+	IOMMU_TEST_OP_DIRTY,
 };
 
 enum {
@@ -96,6 +97,14 @@ struct iommu_test_cmd {
 		struct {
 			__u32 limit;
 		} memory_limit;
+		struct {
+			__u32 flags;
+			__aligned_u64 iova;
+			__aligned_u64 length;
+			__aligned_u64 page_size;
+			__aligned_u64 uptr;
+			__aligned_u64 out_nr_dirty;
+		} dirty;
 	};
 	__u32 last;
 };
