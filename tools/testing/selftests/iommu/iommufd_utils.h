9b4c91f12d97a3106fd0a83fb0999876793dc0f9
Joao Martins <joao.m.martins@oracle.com>
2023-05-12 18:05:22 -0400

    iommufd/selftest: Test IOMMU_HWPT_SET_DIRTY
    Change mock_domain to supporting dirty tracking and add tests to exercise
    the new SET_DIRTY API in the iommufd_dirty_tracking selftest fixture.
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>

--- tools/testing/selftests/iommu/iommufd_utils.h
+++ tools/testing/selftests/iommu/iommufd_utils.h
@@ -177,8 +177,29 @@ static int _test_cmd_access_replace_ioas(int fd, __u32 access_id,
 		return ret;
 	return 0;
 }
+
+
 #define test_cmd_access_replace_ioas(access_id, ioas_id) \
 	ASSERT_EQ(0, _test_cmd_access_replace_ioas(self->fd, access_id, ioas_id))
+
+static int _test_cmd_set_dirty(int fd, __u32 hwpt_id, bool enabled)
+{
+	struct iommu_hwpt_set_dirty cmd = {
+		.size = sizeof(cmd),
+		.flags = enabled ? IOMMU_DIRTY_TRACKING_ENABLED :
+				   IOMMU_DIRTY_TRACKING_DISABLED,
+		.hwpt_id = hwpt_id,
+	};
+	int ret;
+
+	ret = ioctl(fd, IOMMU_HWPT_SET_DIRTY, &cmd);
+	if (ret)
+		return -errno;
+	return 0;
+}
+
+#define test_cmd_set_dirty(hwpt_id, enabled) \
+	ASSERT_EQ(0, _test_cmd_set_dirty(self->fd, hwpt_id, enabled))
 
 static int _test_cmd_create_access(int fd, unsigned int ioas_id,
 				   __u32 *access_id, unsigned int flags)
